351a7c8466246876167d105322e23fd8fa8e86db
Lee Garam <agerio100@naver.com>
2025-02-21 23:11:40 +0900

    Dev: Check and require root privilege at the very start of the program execution

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 // main.go
 package main
 
-import "eetf/cmd"
+import (
+	"fmt"
+	"log"
+	"os/user"
+
+	"eetf/cmd"
+)
+
+// This program requires the root privileges because it reads from /sys folder.
+func isRoot() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("Failed to get current user: %v", err)
+	}
+	return currentUser.Username == "root"
+}
 
 func main() {
+	if !isRoot() {
+		fmt.Println("This program requires root privileges to run.")
+		return
+	}
+
 	cmd.Execute()
 }
